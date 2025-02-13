6c912b33b22c56516cfb34a6316f145549c0dbcd
johnnyboi12 <54293281+JohnSmoit@users.noreply.github.com>
2025-02-13 13:31:47 -0800

    add true false defines for non-windows builds

--- test/shared/platform_utils.h
+++ test/shared/platform_utils.h
@@ -11,6 +11,12 @@ typedef void * FType_Proc;
 
 typedef unsigned int b32;
 
+// TODO: Centralize this next testing suite update please
+#if !defined(TRUE) && !defined(FALSE)
+    #define TRUE 1
+    #define FALSE 0
+#endif
+
 
 HModule platLoadLibrary(HModule mod, const char * name);
 
