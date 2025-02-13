abe4c53c5d795ceba857be7bfb2e8be56c7d934f
johnnyboi12 <54293281+JohnSmoit@users.noreply.github.com>
2025-02-13 12:57:32 -0800

    implement testing helper routines

---
+++ test/shared/platform_utils_Unix.c
@@ -0,0 +1,39 @@
+#include "shared/platform_utils.h"
+
+#include <stdlib.h>
+#include <dlfcn.h>
+
+struct Module {
+    void * hLib;
+};
+
+HModule platLoadLibrary(HModule mod, const char * name) {
+    void * handle = dlopen(name, RTLD_LAZY);
+    if (!handle) {
+        return NULL;
+    }
+
+    mod = (HModule) malloc(sizeof(struct Module));
+    mod->hLib = handle;
+
+    return mod;
+}
+
+b32 platUnloadLibrary(HModule module) {
+    if (!module) {
+        return FALSE;
+    }
+
+    dlclose(module->hLib);
+
+    free(module);
+    return TRUE;
+}
+
+FType_Proc platGetProcAddr(HModule mod, const char * name) {
+    if (!mod) {
+        return NULL;
+    }
+
+    return dlsym(mod->hLib, name);
+}
