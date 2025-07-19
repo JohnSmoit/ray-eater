13c42780542b9a439ac95dd8a7de3c21e1d3fd5e
JohnSmoit <ju.smoit@gmail.com>
2025-07-18 17:06:25 -0700

    chore(zls): update build.zig to give better build information to zls

--- build.zig
+++ build.zig
@@ -1,7 +1,9 @@
 const std = @import("std");
 
-const Module = std.Build.Module;
-const ResolvedTarget = std.Build.ResolvedTarget;
+const Build = std.Build;
+const Module = Build.Module;
+const Compile = Build.Step.Compile;
+const ResolvedTarget = Build.ResolvedTarget;
 const OptimizeMode = std.builtin.OptimizeMode;
 
 const BuildOpts = struct {
@@ -22,7 +24,7 @@ fn resolveGLFWSystemDeps(m: *Module) void {
     m.linkSystemLibrary("shell32", .{});
 }
 // Build project dependencies
-fn buildDeps(b: *std.Build, opts: BuildOpts) Dependencies {
+fn buildDeps(b: *Build, opts: BuildOpts) Dependencies {
     const vulkan_mod = b.dependency(
         "vulkan",
         .{
@@ -54,10 +56,10 @@ fn buildDeps(b: *std.Build, opts: BuildOpts) Dependencies {
 }
 
 fn buildLibrary(
-    b: *std.Build,
+    b: *Build,
     deps: Dependencies,
     opts: BuildOpts,
-) *Module {
+) struct { *Module, *Compile } {
     // module definition for the Ray Eater Library
     // All of the actual "meat" of the renderer is in the library
     const lib_mod = b.createModule(.{
@@ -78,47 +80,34 @@ fn buildLibrary(
 
     b.installArtifact(lib);
 
-    return lib_mod;
+    return .{ lib_mod, lib };
 }
 
 const SampleEntry = struct {
     name: []const u8,
     path: []const u8,
+
+    // these get populated later
+    mod: *Module = undefined,
+    exe: *Compile = undefined,
 };
-const sample_files = [_]SampleEntry{
+var sample_files = [_]SampleEntry{
     .{ .name = "basic_planes", .path = "basic_planes.zig" },
     .{ .name = "basic_compute", .path = "basic_compute.zig" },
     .{ .name = "test_sample", .path = "test_sample.zig" },
 };
-/// build sample applications which demonstrate the usage
-/// and features of the library
-fn buildSamples(
-    b: *std.Build,
+
+fn populateSampleModules(
+    b: *Build,
     lib_mod: *Module,
     deps: Dependencies,
     opts: BuildOpts,
 ) void {
-    const build_sample = b.option(
-        []const u8,
-        "sample",
-        "specify sample to build and run",
-    );
-    if (build_sample) |sample_name| {
-        var sample_path: ?[]const u8 = null;
-
-        for (sample_files) |f| {
-            if (std.mem.order(u8, sample_name, f.name) == .eq) {
-                sample_path = f.path;
-                break;
-            }
-        }
-
-        if (sample_path == null) return;
-
+    for (&sample_files) |*f| {
         const sample_mod = b.createModule(.{
             .root_source_file = b.path(b.pathJoin(&.{
                 "samples",
-                sample_path.?,
+                f.path,
             })),
             .optimize = opts.optimize,
             .target = opts.target,
@@ -128,14 +117,46 @@ fn buildSamples(
         sample_mod.addImport("glfw", deps.glfw);
 
         const sample_exe = b.addExecutable(.{
-            .name = sample_name,
+            .name = f.name,
             .root_module = sample_mod,
             .optimize = opts.optimize orelse .ReleaseFast,
         });
 
-        b.installArtifact(sample_exe);
+        f.mod = sample_mod;
+        f.exe = sample_exe;
+    }
+}
 
-        const run_step = b.addRunArtifact(sample_exe);
+/// build sample applications which demonstrate the usage
+/// and features of the library
+fn buildSamples(
+    b: *Build,
+    lib_mod: *Module,
+    deps: Dependencies,
+    opts: BuildOpts,
+) void {
+    populateSampleModules(b, lib_mod, deps, opts);
+
+    const build_sample = b.option(
+        []const u8,
+        "sample",
+        "specify sample to build and run",
+    );
+    if (build_sample) |sample_name| {
+        var entry: ?*SampleEntry = null;
+
+        for (&sample_files) |*f| {
+            if (std.mem.order(u8, sample_name, f.name) == .eq) {
+                entry = f;
+                break;
+            }
+        }
+
+        if (entry == null) return;
+
+        b.installArtifact(entry.?.exe);
+
+        const run_step = b.addRunArtifact(entry.?.exe);
         run_step.step.dependOn(b.getInstallStep());
 
         const run_cmd = b.step("run", "run a sample executable");
@@ -145,7 +166,7 @@ fn buildSamples(
 
 /// adds a single module and compile step containing
 /// all included tests (based on specified options)
-fn buildTests(b: *std.Build, lib_mod: *Module, deps: Dependencies) void {
+fn buildTests(b: *Build, lib_mod: *Module, deps: Dependencies) void {
     _ = b;
     _ = lib_mod;
     _ = deps;
@@ -153,16 +174,29 @@ fn buildTests(b: *std.Build, lib_mod: *Module, deps: Dependencies) void {
 // Although this function looks imperative, note that its job is to
 // declaratively construct a build graph that will be executed by an external
 // runner.
-pub fn build(b: *std.Build) void {
+pub fn build(b: *Build) void {
     const opts = BuildOpts{
         .target = b.standardTargetOptions(.{}),
         .optimize = b.standardOptimizeOption(.{}),
     };
 
     const deps = buildDeps(b, opts);
-    const lib_mod = buildLibrary(b, deps, opts);
+    const lib_mod, const lib_exe = buildLibrary(b, deps, opts);
 
     // handle samples and testing if specified
     buildSamples(b, lib_mod, deps, opts);
     buildTests(b, lib_mod, deps);
+
+    // zls-friendly check step
+    // (which made all the rest of the code way grosser)
+    // ... sigh
+    const check_step = b.step(
+        "check",
+        "test project build without installing artifacts",
+    );
+
+    check_step.dependOn(&lib_exe.step);
+    for (&sample_files) |f| {
+        check_step.dependOn(&f.exe.step);
+    }
 }
