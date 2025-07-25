4e9d46539e2727ea8d8ac2820f2f88e4517faf86
johnnyboi12 <54293281+JohnSmoit@users.noreply.github.com>
2025-07-24 21:44:46 -0600

    chore(framebuffer): update framebuffer initialization to be parametarized based on a swapchain

--- src/api/frame_buffer.zig
+++ src/api/frame_buffer.zig
@@ -15,34 +15,37 @@ pub const FrameBufferInfo = struct {
 };
 pub const Config = struct {
     renderpass: *const RenderPass,
-    image_views: []const Swapchain.ImageInfo,
+    swapchain: *const Swapchain,
     depth_view: ?vk.ImageView = null,
-    extent: vk.Rect2D,
 };
 
 framebuffers: []vk.Framebuffer,
 pr_dev: *const vk.DeviceProxy,
 allocator: Allocator,
-extent: vk.Rect2D,
+extent: vk.Extent2D,
 
 /// ## Notes
 /// Unfortunately, allocation is neccesary due to the runtime count of the swapchain
 /// images
-pub fn initAlloc(ctx: *const Context, allocator: Allocator, config: *const Config) !Self {
+pub fn initAlloc(ctx: *const Context, allocator: Allocator, config: Config) !Self {
     const pr_dev: *const vk.DeviceProxy = ctx.env(.di);
-    var framebuffers = try allocator.alloc(vk.Framebuffer, config.image_views.len);
+
+    const image_views = config.swapchain.images;
+    const extent = config.swapchain.extent;
+
+    var framebuffers = try allocator.alloc(vk.Framebuffer, image_views.len);
 
     const attachment_count: u32 = if (config.depth_view != null) 2 else 1;
 
-    for (config.image_views, 0..) |*info, index| {
+    for (image_views, 0..) |*info, index| {
         const views = [2]vk.ImageView{ info.h_view, config.depth_view orelse .null_handle };
 
         framebuffers[index] = try pr_dev.createFramebuffer(&.{
             .render_pass = config.renderpass.h_rp,
             .attachment_count = attachment_count,
             .p_attachments = views[0..],
-            .width = config.extent.extent.width,
-            .height = config.extent.extent.height,
+            .width = extent.width,
+            .height = extent.height,
             .layers = 1,
         }, null);
     }
@@ -51,14 +54,17 @@ pub fn initAlloc(ctx: *const Context, allocator: Allocator, config: *const Confi
         .framebuffers = framebuffers,
         .pr_dev = pr_dev,
         .allocator = allocator,
-        .extent = config.extent,
+        .extent = extent,
     };
 }
 
 pub fn get(self: *const Self, image_index: u32) FrameBufferInfo {
     return FrameBufferInfo{
         .h_framebuffer = self.framebuffers[@intCast(image_index)],
-        .extent = self.extent,
+        .extent = vk.Rect2D{
+            .offset = .{ .x = 0, .y = 0 },
+            .extent = self.extent,
+        },
     };
 }
 
