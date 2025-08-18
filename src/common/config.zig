4a88cf2ca9cf18cb77f10d694739963012cf799d
johnnyboi12 <54293281+JohnSmoit@users.noreply.github.com>
2025-08-17 21:35:24 -0700

    feat(config): worked out an API for sensible defaults

---
+++ src/common/config.zig
@@ -0,0 +1,10 @@
+//! Configurations and stuff but cooler
+
+
+pub fn ConfigurationRegistry(
+    comptime ConfigType: type,
+    comptime Profiles: type,
+    comptime profile_defs: anytype,
+) type {
+
+}
